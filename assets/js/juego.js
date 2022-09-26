ae087c100420b622884c495b6a6fbbfdc8a11b38
Israel De León <israel.zacarias@plasticosuni.com>
2022-09-26 16:05:06 -0600

    Se realiza la función de dar valor a cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -63,3 +63,41 @@ const pedirCarta = () => {
 // }
 // deck = [];
 
+// PedirCarta();
+
+// const valorCarta = ( carta ) => {
+    
+//     const valor = carta.substring(0, carta.length - 1);
+
+    // let puntos = 0;
+    // 2 = 2,  10 = 10, 3 = 3
+
+    // if( isNaN( valor ) ){
+        // console.log('No es un número');
+        
+        // puntos = ( valor === 'A' ) ? 11 : 10;
+
+
+    // }else{
+        // console.log('Es un número');
+        // puntos = valor * 1;
+    // }
+    // console.log(puntos)
+// }
+
+// valorCarta('9D'); 
+
+
+// SE PROCEDE A REDUCIR LA FUNCIÓN 
+
+const valorCarta = ( carta ) => {
+    
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN(valor)) ?
+    ( valor === 'A') ? 11 : 10
+    : valor * 1; 
+
+}    
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor });
